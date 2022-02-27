6bb6a948aa042fb764e8672329fbf79a1d6d483a
pedropovedaq <pedro.poveda@perficient.com>
2022-02-26 21:55:29 -0500

    remove certain requirements

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,11 +26,11 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     UsersModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
+        // POSTGRES_HOST: Joi.string().required(),
+        // POSTGRES_PORT: Joi.number().required(),
+        // POSTGRES_USER: Joi.string().required(),
+        // POSTGRES_PASSWORD: Joi.string().required(),
+        // POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION_TIME: Joi.string().required(),
